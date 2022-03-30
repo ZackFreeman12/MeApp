2a9b1a0a2365434f6d1dd1d8e92ce74738bf9c5b
KevinGitSar <74475291+KevinGitSar@users.noreply.github.com>
2022-03-30 01:58:54 -0400

    Add files via upload

---
+++ meappchat/model/ChatUsers.java
@@ -0,0 +1,46 @@
+package ca.meapp.meappchat.model;
+
+import android.os.Parcel;
+import android.os.Parcelable;
+
+public class ChatUsers implements Parcelable {
+    private String username;
+
+    public ChatUsers(String username){
+        this.username = username;
+    }
+
+    protected ChatUsers(Parcel in) {
+        username = in.readString();
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public static final Creator<ChatUsers> CREATOR = new Creator<ChatUsers>() {
+        @Override
+        public ChatUsers createFromParcel(Parcel in) {
+            return new ChatUsers(in);
+        }
+
+        @Override
+        public ChatUsers[] newArray(int size) {
+            return new ChatUsers[size];
+        }
+    };
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel parcel, int i) {
+        parcel.writeString(username);
+    }
+}
